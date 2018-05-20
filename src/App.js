d9b4450a3f5b37f2b3d960d7ad8b59c72c0c168f
V Saravanan <saravanan.resume@gmail.com>
2018-05-21 01:55:07 +0800

    use_map_to_create_react_components_from_arrays_of_data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// Control React Component Updates When New Props Are Received
 
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -7,39 +6,28 @@ import PropTypes from 'prop-types';
 class App extends React.Component {
   constructor() {
     super();
-    this.state = { increasing: false };
-  }
-  update() {
-    ReactDOM.render(
-      <App val={this.props.val + 1} />,
-      document.getElementById('root'));
-  }
-  componentWillReceiveProps(nextProps) {
-    this.setState({ increasing: nextProps.val > this.props.val });
+    this.state = {items: []}
   }
 
-  shouldComponentUpdate(nextProps, nextState) {
-    return nextProps.val % 5 === 0;
+  componentWillMount = () => {
+    fetch('https://swapi.co/api/people/?format=json')
+    .then ( res => res.json())
+    .then (({results: items})  => this.setState({items}))
   }
-
-  componentDidUpdate(prevProps, prevState) {
-    console.log(`prevProps: ${prevProps.val}`);
-  }
-
+  
+  
   render() {
-    console.log(this.state.increasing);
+    let items = this.state.items;
     return (
-      <button onClick={this.update.bind(this)}>
-        {this.props.val}
-      </button>
-    );
+      <div>
+        {items.map(item => 
+        <Person  key={item.name} person={item} />
+        )}
+
+      </div>
+    )
   }
 }
 
-App.propTypes = {
-  val: PropTypes.PropTypes.number,
-};
-
-App.defaultProps = { val: 0 };
-
+const Person = (props) => <h4>{ props.person.name}</h4>
 export default App;
